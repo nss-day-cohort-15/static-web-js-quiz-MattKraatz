e115449c66554737a9775f8518c56c000c256650
Matt Kraatz <matt.kraatz@gmail.com>
2016-07-18 09:01:37 -0500

    ready to submit quiz for grading

--- scripts.js
+++ scripts.js
@@ -4,13 +4,23 @@ submit.addEventListener("click", validateForm)
 treeHeight.addEventListener("keydown", returnCheck)
 treeChar.addEventListener("keydown", returnCheck)
 
+// Assigning input values to an object after event validation and passing to the buildTree function
+
+function treeCapture() {
+  var tree = {
+    height: treeHeight.value,
+    char: treeChar.value
+  };
+  buildTree(tree);
+}
+
 // Function to check whether both input fields are completed
 
 function validateForm() {
   if (treeHeight.value == "" || treeChar.value === "") {
     alert("Please fill out both fields to continue.")
   } else {
-    buildTree();
+    treeCapture();
   }
 }
 
@@ -24,8 +34,8 @@ function returnCheck() {
 
 // Function to build the tree
 
-function buildTree() {
-  for (i = 1; i <= treeHeight.value; i++) {
-    console.log(" ".repeat(treeHeight.value - i) + treeChar.value.repeat((i * 2) - 1) + " ".repeat(treeHeight.value - i))
+function buildTree(tree) {
+  for (i = 1; i <= tree.height; i++) {
+    console.log(" ".repeat(tree.height - i) + tree.char.repeat((i * 2) - 1) + " ".repeat(tree.height - i))
   }
 }
